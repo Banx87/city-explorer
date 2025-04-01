b61735efd370e55a14afb0aa6fcaeb2db640cb3b
Banx87 <joni.lindholm@gmail.com>
2025-04-01 11:37:44 +0300

    [ADD] create a basic layout view for the City explorer application

---
+++ views/layouts/main.view.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./css/simple.css" />
+    <link rel="stylesheet" type="text/css" href="./css/styles.css" />
+    <title>City explorer</title>
+</head>
+
+<body>
+    <header>
+        <h1>
+            <a href="index.php">
+                Cities explorer
+            </a>
+        </h1>
+        <p>Explore and find cities</p>
+
+    </header>
+    <main>
+        <?php echo $contents; ?>
+    </main>
+</body>
+
+</html>
