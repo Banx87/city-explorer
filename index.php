64d36a56d0d2cef76de28e73bd0b8fddef56c806
Banx87 <joni.lindholm@gmail.com>
2025-04-01 11:38:08 +0300

    [ADD] implement WorldCityModel and update index to display city entries

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php
-require __DIR__ . '/inc/all.inc';
+require __DIR__ . '/inc/all.inc.php';
+
+$budapest = new WorldCityModel();
+$budapest->city = 'Budapest';
+$budapest->country = 'Hungary';
+$budapest->population = '3500000';
+
+$helsinki = new WorldCityModel();
+$helsinki->city = 'Helsinki';
+$helsinki->country = 'Finland';
+$helsinki->population = '1500000';
+
+$oslo = new WorldCityModel();
+$oslo->city = 'Oslo';
+$oslo->country = 'Norway';
+$oslo->population = '7500000';
 
 
-render('index.view', []);
+
+$entries = [
+    $budapest,
+    $helsinki,
+    $oslo
+];
+
+render('index.view', ['entries' => $entries]);
